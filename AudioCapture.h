7339251ce018198dcc0ed00404ea2ee3dc197887
KalahariKanga <djfhill@mailinator.com>
2016-07-02 21:57:53 +0100

    retrieve waveform data better

--- AudioCapture.h
+++ AudioCapture.h
@@ -3,13 +3,16 @@
 
 class AudioCapture
 {
-	const int bufferSize = 1024;
+	
 	float* waveform, FFT;
 	DWORD recordHandle;
+	int boost = 10000;
+	const int bufferSize = 1024;
 public:
 	AudioCapture();
 	~AudioCapture();
+	
 	void update();
-	float* getWaveform(){ return waveform; }
+	float getWaveform(float i){ return boost * waveform[(int)(i*bufferSize)]; }
 };
 
--- Generator.cpp
+++ Generator.cpp
@@ -12,10 +12,9 @@ Generator::~Generator()
 
 void Generator::update(Canvas& target)
 {
-	auto waveform = ac->getWaveform();
 	for (int c = 0; c < 640; c++)
 	{
 		target.setDrawColour(sf::Color(c, 255 - c, 200));
-		target.drawLine(c, 240, c, 240 + waveform[c] * 5000);
+		target.drawLine(c, 240, c, 240 + ac->getWaveform((float)c/640));
 	}
 }
